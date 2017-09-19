de23d60abc3afd645da581011cee29e9f53c8595
DeAndre Johnson <dre@phreshr.com>
2017-09-20 01:23:26 +0400

    formating

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,17 +1,17 @@
-import {NgModule, Component} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import { NgModule, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'lazy-view',
+  selector: 'app-lazy-view',
   template: `<h3>i'm lazy</h3>`
 })
-export class LazyComponent {}
+export class LazyComponent { }
 
 @NgModule({
   declarations: [LazyComponent],
   imports: [
     RouterModule.forChild([
-      { path: '', component: LazyComponent, pathMatch: 'full'}
+      { path: '', component: LazyComponent, pathMatch: 'full' }
     ])
   ]
 })
