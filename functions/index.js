74c3ad0cb9a3187114da8f90e52922ed9b60fbd6
DeAndre Johnson <dre@phreshr.com>
2017-09-20 16:32:04 +0400

    caching

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,57 @@
-require('zone.js/dist/zone-node');
+require('zone.js/dist/zone-node')
 
-const functions = require('firebase-functions');
-const express = require('express');
-const path = require('path');
-const { enableProdMode } = require('@angular/core');
-const { renderModuleFactory } = require('@angular/platform-server');
+const functions = require('firebase-functions')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const LRUCache = require('lru-cache')
+const helmet = require('helmet')
+const express = require('express')
+const path = require('path')
+const { enableProdMode } = require('@angular/core')
+const { renderModuleFactory } = require('@angular/platform-server')
 
-const { AppServerModuleNgFactory } = require('./dist-ssr/main.bundle');
+const { AppServerModuleNgFactory } = require('./dist-ssr/main.bundle')
 
-enableProdMode();
+enableProdMode()
+
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 // 1hour
+})
+
+function getCacheKey (req) {
+  return `${req.url}`
+}
 
 const index = require('fs')
   .readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf8')
-  .toString();
+  .toString()
 
-let app = express();
+let app = express()
 
-app.get('**', function(req, res) {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(compression())
+app.use(helmet())
+
+app.get('**', function (req, res) {
+  const key = getCacheKey(req)
+
+  // If we have a page in the cache, let's serve it
+  if (ssrCache.has(key)) {
+    // console.log(`CACHE HIT: ${key}`)
+    res.send(ssrCache.get(key))
+    return
+  }
   renderModuleFactory(AppServerModuleNgFactory, {
     url: req.path,
     document: index
-  }).then(html => res.status(200).send(html));
-});
+  }).then(html => {
+    // Let's cache this page
+    // console.log(`CACHE MISS: ${key}`)
+    ssrCache.set(key, html)
+    res.status(200).send(html)
+  })
+})
 
-exports.ssr = functions.https.onRequest(app);
+exports.ssr = functions.https.onRequest(app)
